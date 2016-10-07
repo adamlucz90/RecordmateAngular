eed6062a3bcd8226a123d0f01e2719624df2d8b7
Adam Lucz <adamlucz90@gmail.com>
2016-10-07 11:07:09 -0500

    working carousel

--- angular/controllers/profileController.js
+++ angular/controllers/profileController.js
@@ -20,21 +20,36 @@ angular.
 			
 			$scope.active = 0;
 			
+			$scope.index = 0;
+			
 			//create variable to hold wishlist items
-			$scope.items;
+			$scope.items = [];
 			
 			//render the wishlist
-			var render = function(){
+			var wishRender = function(){
 					collection.wishlistRender($scope.user).success(function(data){
 					//if there are items fill wishlist, if not show empty wishlist message
 					if(data.items[0]){
-						$scope.items = data.items;
+						$scope.items = data.items.map(function(obj){
+							var item = {
+								artist: obj.artist,
+								album: obj.album,
+								url: obj.url,
+								id: $scope.index
+							};
+							
+							$scope.index++;
+							
+							return item;
+						});
+						console.log($scope.items);
 					}
 					else{
 						$scope.empty = true;
-						$scope.items = [];
 					}
 				});
-		};				
+		};	
+		
+		wishRender();			
 		}]);
 	
